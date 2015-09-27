5ad603eac50b5b228f1dd94112691c703dd929c5
fradex92 <fradex1992@gmail.com>
2015-09-27 21:57:24 +0400

    Накидал первичную логинку

---
+++ web/Scheduler/Scheduler/Models/Autorization/AuthenticationResult.cs
@@ -0,0 +1,38 @@
+﻿
+using System.Dynamic;
+
+namespace Scheduler.Models
+{
+    /// <summary>
+    /// Результат аутентификации
+    /// 
+    /// </summary>
+    public class AuthenticationResult
+    {
+        /// <summary>
+        /// Соощение об ошибке
+        /// 
+        /// </summary>
+        public string Error { get; set; }
+        /// <summary>
+        /// Флаг успешной аутентификации
+        /// 
+        /// </summary>
+        public bool Success { get; set; }
+        /// <summary>
+        /// Идентификатор пользователя
+        /// 
+        /// </summary>
+        public long UserId { get; set; }
+        /// <summary>
+        /// Имя пользователя
+        /// 
+        /// </summary>
+        public string UserName { get; set; }
+        /// <summary>
+        /// Данные учетной записи пользователя
+        /// 
+        /// </summary>
+        public ExpandoObject UserData { get; set; }
+    }
+}
