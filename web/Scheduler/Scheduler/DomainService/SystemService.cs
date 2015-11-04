03abf698f5aa8ac7c420a932010d5e85bfb85aef
Максим Задворнов <tirelx@outlook.com>
2015-11-04 12:46:18 +0300

    * Добавлены методы получения и сохранения расписаний

--- web/Scheduler/Scheduler/DomainService/SystemService.cs
+++ web/Scheduler/Scheduler/DomainService/SystemService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,12 +11,54 @@ namespace Scheduler.DomainService
 {
     public class SystemService
     {
-        //public List<ApplicationUser> GetUsers()
-        //{
-        //    List< ApplicationUser >
-        //    using (var context = new OwnerDbContext())
-        //    {
-        //    }
-        //}
+        /// <summary>
+        /// Получение всех расписаний
+        /// </summary>
+        /// <returns></returns>
+        public List<UserSchedule> GetAllUserSchedules()
+        {
+            using (var db = new AppDbContext())
+            {
+                return db.UserSchedule.ToList();
+            }
+        }
+        /// <summary>
+        /// Получение расписания по идентификатору
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        public UserSchedule GetUserScheduleByUserId(int UserId)
+        {
+            using (var db = new AppDbContext())
+            {
+                return db.UserSchedule.
+                    FirstOrDefault(x => x.UserId.Id == UserId.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Сохранение/Добавление списка расписаний
+        /// </summary>
+        /// <param name="schedules"></param>
+        /// <returns></returns>
+        public string SaveListUserSchedule(List<UserSchedule> schedules)
+        {
+            using (var db = new AppDbContext())
+            {
+                try
+                {
+                    foreach (var schedule in schedules)
+                    {
+                        db.UserSchedule.AddOrUpdate(x => x.Id, schedule);
+                    }
+                    db.SaveChanges();
+                    return "Данные успешно сохранены!";
+                }
+                catch (Exception)
+                {
+                    return "Ошибка при сохранении данных!";
+                }
+            }
+        }
     }
 }
