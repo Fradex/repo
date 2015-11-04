4ceb6cc612a280426de391954c47f27c2a24983f
Максим Задворнов <tirelx@outlook.com>
2015-11-04 12:27:53 +0300

    * закомментил up в firsr migration

--- web/Scheduler/Scheduler/Migrations/201510081827523_First.cs
+++ web/Scheduler/Scheduler/Migrations/201510081827523_First.cs
@@ -8,87 +8,87 @@ namespace Scheduler.Migrations
         public override void Up()
         {
             
-            CreateTable(
-                "dbo.AspNetRoles",
-                c => new
-                    {
-                        Id = c.String(nullable: false, maxLength: 128),
-                        Name = c.String(nullable: false, maxLength: 256),
-                    })
-                .PrimaryKey(t => t.Id)
-                .Index(t => t.Name, unique: true, name: "RoleNameIndex");
+            //CreateTable(
+            //    "dbo.AspNetRoles",
+            //    c => new
+            //        {
+            //            Id = c.String(nullable: false, maxLength: 128),
+            //            Name = c.String(nullable: false, maxLength: 256),
+            //        })
+            //    .PrimaryKey(t => t.Id)
+            //    .Index(t => t.Name, unique: true, name: "RoleNameIndex");
             
-            CreateTable(
-                "dbo.AspNetUserRoles",
-                c => new
-                    {
-                        UserId = c.String(nullable: false, maxLength: 128),
-                        RoleId = c.String(nullable: false, maxLength: 128),
-                    })
-                .PrimaryKey(t => new { t.UserId, t.RoleId })
-                .ForeignKey("dbo.AspNetRoles", t => t.RoleId, cascadeDelete: true)
-                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
-                .Index(t => t.UserId)
-                .Index(t => t.RoleId);
+            //CreateTable(
+            //    "dbo.AspNetUserRoles",
+            //    c => new
+            //        {
+            //            UserId = c.String(nullable: false, maxLength: 128),
+            //            RoleId = c.String(nullable: false, maxLength: 128),
+            //        })
+            //    .PrimaryKey(t => new { t.UserId, t.RoleId })
+            //    .ForeignKey("dbo.AspNetRoles", t => t.RoleId, cascadeDelete: true)
+            //    .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+            //    .Index(t => t.UserId)
+            //    .Index(t => t.RoleId);
             
-            CreateTable(
-                "dbo.AspNetUsers",
-                c => new
-                    {
-                        Id = c.String(nullable: false, maxLength: 128),
-                        RegisterDate = c.DateTime(),
-                        Email = c.String(maxLength: 256),
-                        EmailConfirmed = c.Boolean(nullable: false),
-                        PasswordHash = c.String(),
-                        SecurityStamp = c.String(),
-                        PhoneNumber = c.String(),
-                        PhoneNumberConfirmed = c.Boolean(nullable: false),
-                        TwoFactorEnabled = c.Boolean(nullable: false),
-                        LockoutEndDateUtc = c.DateTime(),
-                        LockoutEnabled = c.Boolean(nullable: false),
-                        AccessFailedCount = c.Int(nullable: false),
-                        UserName = c.String(nullable: false, maxLength: 256),
-                        Role_Id = c.Int(),
-                    })
-                .PrimaryKey(t => t.Id)
-                .ForeignKey("dbo.UserRoles", t => t.Role_Id)
-                .Index(t => t.UserName, unique: true, name: "UserNameIndex")
-                .Index(t => t.Role_Id);
+            //CreateTable(
+            //    "dbo.AspNetUsers",
+            //    c => new
+            //        {
+            //            Id = c.String(nullable: false, maxLength: 128),
+            //            RegisterDate = c.DateTime(),
+            //            Email = c.String(maxLength: 256),
+            //            EmailConfirmed = c.Boolean(nullable: false),
+            //            PasswordHash = c.String(),
+            //            SecurityStamp = c.String(),
+            //            PhoneNumber = c.String(),
+            //            PhoneNumberConfirmed = c.Boolean(nullable: false),
+            //            TwoFactorEnabled = c.Boolean(nullable: false),
+            //            LockoutEndDateUtc = c.DateTime(),
+            //            LockoutEnabled = c.Boolean(nullable: false),
+            //            AccessFailedCount = c.Int(nullable: false),
+            //            UserName = c.String(nullable: false, maxLength: 256),
+            //            Role_Id = c.Int(),
+            //        })
+            //    .PrimaryKey(t => t.Id)
+            //    .ForeignKey("dbo.UserRoles", t => t.Role_Id)
+            //    .Index(t => t.UserName, unique: true, name: "UserNameIndex")
+            //    .Index(t => t.Role_Id);
             
-            CreateTable(
-                "dbo.AspNetUserClaims",
-                c => new
-                    {
-                        Id = c.Int(nullable: false, identity: true),
-                        UserId = c.String(nullable: false, maxLength: 128),
-                        ClaimType = c.String(),
-                        ClaimValue = c.String(),
-                    })
-                .PrimaryKey(t => t.Id)
-                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
-                .Index(t => t.UserId);
+            //CreateTable(
+            //    "dbo.AspNetUserClaims",
+            //    c => new
+            //        {
+            //            Id = c.Int(nullable: false, identity: true),
+            //            UserId = c.String(nullable: false, maxLength: 128),
+            //            ClaimType = c.String(),
+            //            ClaimValue = c.String(),
+            //        })
+            //    .PrimaryKey(t => t.Id)
+            //    .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+            //    .Index(t => t.UserId);
             
-            CreateTable(
-                "dbo.AspNetUserLogins",
-                c => new
-                    {
-                        LoginProvider = c.String(nullable: false, maxLength: 128),
-                        ProviderKey = c.String(nullable: false, maxLength: 128),
-                        UserId = c.String(nullable: false, maxLength: 128),
-                    })
-                .PrimaryKey(t => new { t.LoginProvider, t.ProviderKey, t.UserId })
-                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
-                .Index(t => t.UserId);
+            //CreateTable(
+            //    "dbo.AspNetUserLogins",
+            //    c => new
+            //        {
+            //            LoginProvider = c.String(nullable: false, maxLength: 128),
+            //            ProviderKey = c.String(nullable: false, maxLength: 128),
+            //            UserId = c.String(nullable: false, maxLength: 128),
+            //        })
+            //    .PrimaryKey(t => new { t.LoginProvider, t.ProviderKey, t.UserId })
+            //    .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+            //    .Index(t => t.UserId);
             
-            CreateTable(
-                "dbo.UserRoles",
-                c => new
-                    {
-                        Id = c.Int(nullable: false, identity: true),
-                        Role = c.String(),
-                        UserId = c.Int(nullable: false),
-                    })
-                .PrimaryKey(t => t.Id);
+            //CreateTable(
+            //    "dbo.UserRoles",
+            //    c => new
+            //        {
+            //            Id = c.Int(nullable: false, identity: true),
+            //            Role = c.String(),
+            //            UserId = c.Int(nullable: false),
+            //        })
+            //    .PrimaryKey(t => t.Id);
             
         }
         
